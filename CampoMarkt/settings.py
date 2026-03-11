06abe6dee027ea4c6fd4a200098591c52db5a547
luis <luisburgos12anzola>
2026-03-11 13:22:23 -0500

    cambios

--- CampoMarkt/settings.py
+++ CampoMarkt/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8l^d=)_fj7mv0r^nnkkc4$c9qti(&%0(v!9lj8ojm3b!s8=@^r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
