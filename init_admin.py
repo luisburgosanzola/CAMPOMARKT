19e7de07af1610262529246431857271523a30a2
luis <luisburgos12anzola>
2026-03-25 20:59:42 -0500

    crear admin

--- init_admin.py
+++ init_admin.py
@@ -2,10 +2,10 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-if not User.objects.filter(username="luis1234").exists():
+if not User.objects.filter(username="tigre1234").exists():
     User.objects.create_superuser(
-        username="luis1234",
-        email="luis1234min@gmail.com",
+        username="tigre1234",
+        email="tigre@gmail.com",
         password="Tigre0250."
     )
     print("ADMIN CREADO")
